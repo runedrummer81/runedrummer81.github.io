a1623d89c4c900201a4b20825f9873c06f5e215f
runedrummer81 <runedrummer81@gmail.com>
2025-09-03 10:28:35 +0200

    virk 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ import Contact from "./pages/Contact";
 export default function App() {
   return (
     <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "runefrisch"}>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
