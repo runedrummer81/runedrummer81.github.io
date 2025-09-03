afac098de117c6301d7dc942866023735e8c59db
runedrummer81 <runedrummer81@gmail.com>
2025-09-03 10:27:56 +0200

    virk1

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "runefrisch"}>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
